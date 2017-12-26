aeea8d44014de332860693402b504d1380742888
Gavishiddappa <gmgmadagi@gmail.com>
2017-12-27 00:44:34 +0530

    Enabled Spring security for the website

---
+++ src/main/java/com/devopsbuddy/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.devopsbuddy.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Hi on 27-12-2017.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /** Public URLs */
+    public static final  String[] PUBLIC_MATCHERS = {
+            "webjars/**",
+            "/css/**",
+            "/js/**",
+            "/images/**",
+            "/",
+            "/about/**",
+            "/contact/**",
+            "/error/**/*",
+    };
+
+    @Override
+    protected  void  configure(HttpSecurity http) throws  Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/payload")
+                .failureUrl("/login?error").permitAll()
+                .and()
+                .logout().permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws  Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password")
+                .roles("USER");
+    }
+}
